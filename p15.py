7dac90a96abe7c6356805beba2ecb8e9977a8f36
zengxm36 <zengxm36@mail2.sysu.edu.cn>
2023-12-10 00:15:12 +0800

    1

--- p15.py
+++ p15.py
@@ -24,7 +24,15 @@ fact_iter(2)
 #汉诺塔的移动可以用递归函数非常简单地实现。
 
 #请编写move(n, a, b, c)函数，它接收参数n
-#表示3个柱子A、B、C中第1个柱子A的盘子数量，然后打印出把所有盘子从A借助B移动到C的方法，例如：
+#n表示3个柱子A、B、C中第1个柱子A的盘子数量，然后打印出把所有盘子从A借助B移动到C的方法，例如：
 
-def move(n,a,b,c):
-    
+
+def move(n, a, b, c):
+    if n == 1:
+        print(a, '-->', c)
+    else:
+        move(n-1, a, c, b)
+        move(1, a, b, c)
+        move(n-1, b, a, c)
+
+move(5,'a','b','c')
